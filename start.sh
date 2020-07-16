40385bd61bf8802bf81c8c5c4380d3a4e787bcce
tounga <tounga.franck-extern@renault.com>
2020-07-16 12:24:38 +0200

    Dockerize application

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#docker-compose down --rmi all
+docker-compose down
+echo "docker-compose up"
+docker-compose up -d
+# tounga.franck@ng-itconsulting.com
+# force rebuild all image
+#docker-compose up --force-recreate --build
